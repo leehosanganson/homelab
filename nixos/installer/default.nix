d3824009fb988ba02274e26bf3bfb3502503433c
Anson Lee <leehosanganson@gmail.com>
2026-05-08 07:56:06 +0100

    (refactor) nixos: extract users block to shared module, add mac-mini key

--- nixos/installer/default.nix
+++ nixos/installer/default.nix
@@ -13,11 +13,11 @@
     };
   };
 
-  users.users.root = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOuRvc3yYsvjGSLlvtiSTGYx8YscOGAxuLoQEgP/llb leehosanganson@gmail.com"
-    ];
-  };
+  # Root access for provisioning (SSH key-based)
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOuRvc3yYsvjGSLlvtiSTGYx8YscOGAxuLoQEgP/llb lhs-desktop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLmxFZ+MJIFIMc/t3bY/EzbN6io/c2lZw1Ab9R68NJk mac-mini"
+  ];
 
   # QEMU guest agent so Proxmox can report IP addresses and manage power state
   services.qemuGuest.enable = true;
