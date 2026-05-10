53ffd8f15c7a878ad3c506076750b2a53befeaf3
Anson Lee <leehosanganson@gmail.com>
2026-05-10 20:20:25 +0100

    update haproxy-1

--- nixos/hosts/haproxy-1/default.nix
+++ nixos/hosts/haproxy-1/default.nix
@@ -6,7 +6,7 @@
     ../../modules/sops-bootstrap.nix
   ];
 
-  system.stateVersion = "25.11";
+  system.stateVersion = "26.05";
 
   networking = {
     hostName = "haproxy-1";
@@ -33,15 +33,6 @@
         group = "acme";
       };
     };
-  };
-
-  # user
-  users.users.ansonlee = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "haproxy" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOuRvc3yYsvjGSLlvtiSTGYx8YscOGAxuLoQEgP/llb lhs-desktop"
-    ];
   };
 
   # ssh
